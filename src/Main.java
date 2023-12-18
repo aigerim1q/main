1b2fb7d1c2cd3df0c5d6119c2b7c478c7c2df6e9
Toma <230434@astanait.edu.kz>
2023-12-18 08:33:09 +0600

    first init

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+
         Scanner in = new Scanner(System.in);
         Scanner in2 = new Scanner(System.in);
         int a = in.nextInt();
