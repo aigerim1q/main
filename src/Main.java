4779dbb1952dd009bf0bbcd2e6bf4b99bca76a76
Toma <230434@astanait.edu.kz>
2023-12-18 08:32:20 +0600

    first init

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        Scanner in2 = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        System.out.println(a+b);
+    }
+}
